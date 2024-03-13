a3d1c9e2aafbf7bf83072999735101fe03022291
Choi YoonChan <choyc9239@gmail.com>
2024-03-13 12:25:13 +0900

    [Feat/#6] ActivityMainBinding 변수 선언 수정

--- app/src/main/java/com/abloom/marry/MainActivity.kt
+++ app/src/main/java/com/abloom/marry/MainActivity.kt
@@ -9,10 +9,13 @@ import androidx.databinding.DataBindingUtil
 import com.abloom.marry.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
