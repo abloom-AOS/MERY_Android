2ada17d273562e26951fe352b85f9c4880bf0e76
Choi YoonChan <choyc9239@gmail.com>
2024-03-13 13:53:52 +0900

    [Feat/#6] lazy을 통한 binding 변수 선언 수정

--- app/src/main/java/com/abloom/marry/MainActivity.kt
+++ app/src/main/java/com/abloom/marry/MainActivity.kt
@@ -10,12 +10,14 @@ import com.abloom.marry.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
