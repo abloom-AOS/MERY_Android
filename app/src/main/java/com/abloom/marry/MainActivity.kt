d8e34240582cf1fc32ac230d14c9d5b9f02852e8
Choi YoonChan <choyc9239@gmail.com>
2024-03-13 14:25:26 +0900

    [Feat/#6] 뷰 페딩 바인딩 객체 변수 참조 수정

--- app/src/main/java/com/abloom/marry/MainActivity.kt
+++ app/src/main/java/com/abloom/marry/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
