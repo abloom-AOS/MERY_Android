0c8cf144a576154c640c312ece1988b486f91671
leeugun123 <leeugun123@naver.com>
2024-03-15 17:34:28 +0900

    Ref: 데이터 바인딩 처리

--- app/src/main/java/com/abloom/marry/HomeActivity.kt
+++ app/src/main/java/com/abloom/marry/HomeActivity.kt
@@ -17,13 +17,13 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        applyWindowInsetsPadding(findViewById(R.id.main))
+        setContentView(binding.root)
+        applyWindowInsetsPadding()
 
     }
 
-    private fun applyWindowInsetsPadding(view: View) {
-        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
+    private fun applyWindowInsetsPadding() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
