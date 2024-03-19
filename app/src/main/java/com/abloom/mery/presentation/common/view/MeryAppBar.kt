144f8d23c3d84e340320ecd8f3e32796d8770028
ki960213 <ki960213@gmail.com>
2024-03-19 11:04:57 +0900

    [Feat/#24] 홈 이외의 화면에서 사용될 앱바 생성

---
+++ app/src/main/java/com/abloom/mery/presentation/common/view/MeryAppBar.kt
@@ -0,0 +1,10 @@
+package com.abloom.mery.presentation.common.view
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MeryAppBar @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+) : ConstraintLayout(context, attrs)
