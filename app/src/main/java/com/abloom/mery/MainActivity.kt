f2fe4229fa98f3c1dfa00714f1d36869a07081a2
ki960213 <ki960213@gmail.com>
2024-03-19 23:37:00 +0900

    [Feat/#23] home fragment일 때만 밝은 배경, 그렇지 않으면 어두운 배경으로 변하도록 DestinationChangedListener 추가

--- app/src/main/java/com/abloom/mery/MainActivity.kt
+++ app/src/main/java/com/abloom/mery/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.abloom.mery
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.addCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toDrawable
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.abloom.mery.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -27,9 +31,31 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         setupBackPressed()
+        setupDestinationChangedListener()
     }
 
     private fun setupBackPressed() {
         onBackPressedDispatcher.addCallback { findNavController(R.id.nav_host_fragment).popBackStack() }
+    }
+
+    // TODO("추후 애니메이션으로 배경이 서서히 변하도록 수정할 예정")
+    private fun setupDestinationChangedListener() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.navController.addOnDestinationChangedListener { _, dest, _ ->
+            if (dest.id == R.id.homeFragment) {
+                binding.root.background = getColor(R.color.home_background).toDrawable()
+                WindowCompat.getInsetsController(
+                    window,
+                    window.decorView
+                ).isAppearanceLightStatusBars = true
+            } else {
+                binding.root.background = Color.BLACK.toDrawable()
+                WindowCompat.getInsetsController(
+                    window,
+                    window.decorView
+                ).isAppearanceLightStatusBars = false
+            }
+        }
     }
 }
