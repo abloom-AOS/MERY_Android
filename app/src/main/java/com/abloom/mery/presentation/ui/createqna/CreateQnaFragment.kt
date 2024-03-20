3f9947546b6e976d91ec789909303651e4d53686
Choi YoonChan <choyc9239@gmail.com>
2024-03-20 22:41:41 +0900

    [Feat/#26] 문답 생성 구현: CreateQnaFragment, Frament_create_qna.xml 파일 추가

--- app/src/main/java/com/abloom/mery/presentation/ui/createqna/CreateQnaFragment.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/createqna/CreateQnaFragment.kt
@@ -1,7 +1,9 @@
 package com.abloom.mery.presentation.ui.createqna
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 import com.abloom.mery.R
 import com.abloom.mery.databinding.FragmentCreateQnaBinding
@@ -12,18 +14,43 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class CreateQnaFragment : BaseFragment<FragmentCreateQnaBinding>(R.layout.fragment_create_qna) {
 
+    private val recommendQuestion by lazy {"동해물과 백둥산이...(추천 데이터)"}
+    private val data by lazy { "해당 질문 id값" }
+    private val key by lazy { "질문 카테고리 종류 key값" }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.appbarCreateQna.setOnNavigationClick {
             findNavController().popBackStack()
         }
-        binding.button2.setOnClickListener {
-            findNavController().navigate(CreateQnaFragmentDirections.actionCreateQnaFragmentToCategoryFragment())
-        }
-        binding.button3.setOnClickListener {
-            findNavController().navigate(
-                CreateQnaFragmentDirections.actionGlobalWriteAnswerFragment(1)
-            )
-        }
+        initData()
+        initListener()
     }
+
+    private fun initData(){
+        binding.tvTodayQuestion.text = recommendQuestion
+    }
+
+    private fun initListener(){
+        binding.ivEconomy.setOnClickListener { goCategoryFragment(key, data) }
+        binding.ivCommunication.setOnClickListener {goCategoryFragment(key,data)}
+        binding.ivValues.setOnClickListener { goCategoryFragment(key,data) }
+        binding.ivLife.setOnClickListener { goCategoryFragment(key,data) }
+        binding.ivChildren.setOnClickListener { goCategoryFragment(key,data) }
+        binding.ivFamily.setOnClickListener { goCategoryFragment(key,data) }
+        binding.ivMarried.setOnClickListener { goCategoryFragment(key,data) }
+        binding.ivHealth.setOnClickListener { goCategoryFragment(key,data) }
+        binding.ivWedding.setOnClickListener { goCategoryFragment(key,data) }
+        binding.ivFuture.setOnClickListener { goCategoryFragment(key,data) }
+        binding.ivPresent.setOnClickListener { goCategoryFragment(key,data) }
+        binding.ivPast.setOnClickListener { goCategoryFragment(key,data) }
+    }
+
+
+    private fun goCategoryFragment(key: String, data: String){
+        val bundle = bundleOf(key to data)
+        findNavController().navigate(R.id.action_createQnaFragment_to_categoryFragment,bundle)
+    }
+
 }
