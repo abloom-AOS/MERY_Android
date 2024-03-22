1c346fa49fd1efdab6a47f69344a5bf615ae5e5f
Choi YoonChan <choyc9239@gmail.com>
2024-03-22 22:13:26 +0900

    [Feat/#26] requestUserInfo()함수 가독성을 위해 코드  수정

--- app/src/main/java/com/abloom/mery/createqnaretest/IdRepository.kt
+++ app/src/main/java/com/abloom/mery/createqnaretest/IdRepository.kt
@@ -6,20 +6,17 @@ import kotlinx.coroutines.async
 
 class IdRepository() {
 
-    private suspend fun requestUserInfo(): UserInfo {
-        return UserInfo(1, arrayListOf("질문1","질문2","질문3","질문4","질문5"))
-    }
+    private suspend fun requestUserInfo(): UserInfo =
+        UserInfo(1, arrayListOf("질문1", "질문2", "질문3", "질문4", "질문5"))
 
     suspend fun requestRandomQuestion(): String {
         val userInfo = requestUserInfo()
-
         val questions = userInfo.questions
         return questions.random()
     }
 
     suspend fun requestRandomId(): Long {
         val userInfo = requestUserInfo()
-
         val id = userInfo.questionId
         return id
     }
