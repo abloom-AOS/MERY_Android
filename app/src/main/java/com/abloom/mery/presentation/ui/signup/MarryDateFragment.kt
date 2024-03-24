df51ed0b424a372c6f866d2ba6322785b3fed930
leeugun123 <leeugun123@naver.com>
2024-03-24 21:50:38 +0900

    [Feat/#35]: MarryDateFragment 클래스 및 xml 생성

---
+++ app/src/main/java/com/abloom/mery/presentation/ui/signup/MarryDateFragment.kt
@@ -0,0 +1,21 @@
+package com.abloom.mery.presentation.ui.signup
+
+import android.os.Bundle
+import android.view.View
+import com.abloom.mery.R
+import com.abloom.mery.databinding.FragmentMarryDateBinding
+import com.abloom.mery.presentation.common.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MarryDateFragment : BaseFragment<FragmentMarryDateBinding>(R.layout.fragment_marry_date) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+
+}
