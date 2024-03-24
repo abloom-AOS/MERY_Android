a8f1dac9b2b168a480d83d2ae9d202bdc821aea8
leeugun123 <leeugun123@naver.com>
2024-03-24 22:25:13 +0900

    [Feat/#35]: Appbar binding init 구현

--- app/src/main/java/com/abloom/mery/presentation/ui/signup/MarryDateFragment.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/signup/MarryDateFragment.kt
@@ -2,9 +2,12 @@ package com.abloom.mery.presentation.ui.signup
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.abloom.mery.R
 import com.abloom.mery.databinding.FragmentMarryDateBinding
 import com.abloom.mery.presentation.common.base.BaseFragment
+import com.abloom.mery.presentation.common.view.setOnActionClick
+import com.abloom.mery.presentation.common.view.setOnNavigationClick
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -14,6 +17,19 @@ class MarryDateFragment : BaseFragment<FragmentMarryDateBinding>(R.layout.fragme
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initListener()
+
+    }
+
+    private fun initListener() {
+
+        binding.appbarMarryDate.setOnNavigationClick {
+            findNavController().popBackStack()
+        }
+
+        binding.appbarMarryDate.setOnActionClick {
+            //  TODO("데이터 업데이트 후 Signup/Step03(이름정보입력)로 이동 하는 로직 구현")
+        }
 
     }
 
