cf0e6f29baff02af9e4560d1e7037b103afa7dc8
ki960213 <ki960213@gmail.com>
2024-03-25 12:55:45 +0900

    [Chore/#34] junit5와 assertj 의존성 추가 및 Junit 테스트 설정

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -7,3 +7,14 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter.api)
+    testImplementation(libs.assertj.core)
+    testImplementation(libs.junit.jupiter.params)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+}
