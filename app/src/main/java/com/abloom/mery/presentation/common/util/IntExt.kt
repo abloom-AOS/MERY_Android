075ea76b0e995389056fee8ae7eed0b303b2a867
Choi YoonChan <choyc9239@gmail.com>
2024-03-26 01:29:32 +0900

    [Feat/#36] categoryFragment의 tablayout의 탭 선택 리스너를 확장함수 추가

--- app/src/main/java/com/abloom/mery/presentation/common/util/IntExt.kt
+++ app/src/main/java/com/abloom/mery/presentation/common/util/IntExt.kt
@@ -1,7 +1,22 @@
 package com.abloom.mery.presentation.common.util
 
 import android.content.res.Resources
+import android.widget.Toast
+import com.google.android.material.tabs.TabLayout
 import kotlin.math.roundToInt
 
 val Int.dp: Int
     get() = (this * Resources.getSystem().displayMetrics.density).roundToInt()
+
+
+fun TabLayout.onTabSelected(onTabSelected:(tab: TabLayout.Tab) -> Unit){
+    this.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+        override fun onTabSelected(tab: TabLayout.Tab) {
+            onTabSelected.invoke(tab)
+        }
+
+        override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+        override fun onTabReselected(tab: TabLayout.Tab?) {}
+    })
+}
