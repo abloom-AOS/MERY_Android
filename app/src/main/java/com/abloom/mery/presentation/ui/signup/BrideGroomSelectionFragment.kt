f461d62fa9fb8b730898d5f65fe8fe81eb3b5507
leeugun123 <leeugun123@naver.com>
2024-03-25 21:43:19 +0900

    [Feat/#35]: 코드 정렬

--- app/src/main/java/com/abloom/mery/presentation/ui/signup/BrideGroomSelectionFragment.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/signup/BrideGroomSelectionFragment.kt
@@ -16,7 +16,6 @@ class BrideGroomSelectionFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initListener()
-
     }
 
     private fun initListener() {
