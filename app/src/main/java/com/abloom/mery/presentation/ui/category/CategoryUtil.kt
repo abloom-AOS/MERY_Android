25e571ca7c6d03bb6d2dc37473c0e5dcc6bcb4d1
Choi YoonChan <choyc9239@gmail.com>
2024-03-26 22:55:37 +0900

    [Feat/#36] 질문카테고리 UI CategoryUtil로 따로 확장함수 로직 빼기

---
+++ app/src/main/java/com/abloom/mery/presentation/ui/category/CategoryUtil.kt
@@ -0,0 +1,15 @@
+package com.abloom.mery.presentation.ui.category
+
+import com.google.android.material.tabs.TabLayout
+
+fun TabLayout.onTabSelected(onTabSelected:(tab: TabLayout.Tab) -> Unit){
+    this.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+        override fun onTabSelected(tab: TabLayout.Tab) {
+            onTabSelected(tab)
+        }
+
+        override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+        override fun onTabReselected(tab: TabLayout.Tab?) {}
+    })
+}
