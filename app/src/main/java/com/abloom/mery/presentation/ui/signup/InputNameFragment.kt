7bd1c9c44b8b7f28a21ade1c70ed70fb5a08959e
leeugun123 <leeugun123@naver.com>
2024-03-26 22:23:48 +0900

    [Feat/#41]: InputNameFragment 생성

---
+++ app/src/main/java/com/abloom/mery/presentation/ui/signup/InputNameFragment.kt
@@ -0,0 +1,18 @@
+package com.abloom.mery.presentation.ui.signup
+
+import android.os.Bundle
+import android.view.View
+import com.abloom.mery.R
+import com.abloom.mery.databinding.FragmentInputNameBinding
+import com.abloom.mery.presentation.common.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class InputNameFragment : BaseFragment<FragmentInputNameBinding>(R.layout.fragment_input_name) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
