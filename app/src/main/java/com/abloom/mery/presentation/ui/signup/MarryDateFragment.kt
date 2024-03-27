a8615252e3e8d9252389997e0b95dda7097e76c7
leeugun123 <leeugun123@naver.com>
2024-03-27 16:41:20 +0900

    [Design/#41]: wheelDatePicker 적용

--- app/src/main/java/com/abloom/mery/presentation/ui/signup/MarryDateFragment.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/signup/MarryDateFragment.kt
@@ -1,12 +1,14 @@
 package com.abloom.mery.presentation.ui.signup
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.activityViewModels
 import com.abloom.mery.R
 import com.abloom.mery.databinding.FragmentMarryDateBinding
 import com.abloom.mery.presentation.common.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import sh.tyy.wheelpicker.DatePickerView
 
 @AndroidEntryPoint
 class MarryDateFragment : BaseFragment<FragmentMarryDateBinding>(R.layout.fragment_marry_date) {
@@ -16,6 +18,10 @@ class MarryDateFragment : BaseFragment<FragmentMarryDateBinding>(R.layout.fragme
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.datePicker.setWheelListener(object : DatePickerView.Listener {
+            override fun didSelectData(year: Int, month: Int, day: Int) {
+                Log.e("TAG", "$year $month $day")
+            }
+        })
     }
-
 }
