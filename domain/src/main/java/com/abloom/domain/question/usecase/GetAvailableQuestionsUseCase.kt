234b2fcbbb1c5cb8bfb344063dd2983637df28f7
ki960213 <ki960213@gmail.com>
2024-03-27 18:02:28 +0900

    [Feat/#34] 질문 관련 유스케이스 구현

---
+++ domain/src/main/java/com/abloom/domain/question/usecase/GetAvailableQuestionsUseCase.kt
@@ -0,0 +1,32 @@
+package com.abloom.domain.question.usecase
+
+import com.abloom.domain.qna.model.Qna
+import com.abloom.domain.qna.repository.ProspectiveCoupleQnaRepository
+import com.abloom.domain.question.model.Category
+import com.abloom.domain.question.model.Question
+import com.abloom.domain.question.repository.QuestionRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GetAvailableQuestionsUseCase @Inject constructor(
+    private val questionRepository: QuestionRepository,
+    private val qnaRepository: ProspectiveCoupleQnaRepository
+) {
+
+    /**
+     * Qna로 만들지 않은 질문만 사용가능합니다.
+     */
+    @OptIn(ExperimentalCoroutinesApi::class)
+    operator fun invoke(): Flow<Map<Category, Question>> =
+        qnaRepository.getQnas().flatMapLatest { qnas ->
+            val unavailableQuestionIds = qnas.map(Qna::questionId).toSet()
+
+            questionRepository.getQuestions().map { questions ->
+                questions.filter { question -> question.id !in unavailableQuestionIds }
+                    .associateBy { question -> question.category }
+            }
+        }
+}
