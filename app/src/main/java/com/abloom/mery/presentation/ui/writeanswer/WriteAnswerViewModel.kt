66852340d8e934e2eff9cb1b5e0913b3857f697f
ki960213 <ki960213@gmail.com>
2024-03-27 20:28:36 +0900

    [Feat/#34] WriteAnswerViewModel에서 GetQnaUseCase 대신 GetQuestionUseCase를 사용하도록 변경

--- app/src/main/java/com/abloom/mery/presentation/ui/writeanswer/WriteAnswerViewModel.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/writeanswer/WriteAnswerViewModel.kt
@@ -4,42 +4,31 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.abloom.domain.qna.model.Answer
-import com.abloom.domain.qna.model.Qna
 import com.abloom.domain.qna.usecase.AnswerQnaUseCase
-import com.abloom.domain.qna.usecase.GetQnaUseCase
 import com.abloom.domain.question.model.Question
+import com.abloom.domain.question.usecase.GetQuestionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 @HiltViewModel
 class WriteAnswerViewModel(
     savedStateHandle: SavedStateHandle,
-    getQnaUseCase: GetQnaUseCase,
+    getQuestionUseCase: GetQuestionUseCase,
     private val answerQnaUseCase: AnswerQnaUseCase
 ) : ViewModel() {
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    private val qna: StateFlow<Qna?> = savedStateHandle
+    val question: StateFlow<Question?> = savedStateHandle
         .getStateFlow("question_id", INVALID_QUESTION_ID)
         .filter { it != INVALID_QUESTION_ID }
-        .flatMapLatest { getQnaUseCase(it) }
-        .stateIn(
-            initialValue = null,
-            started = SharingStarted.WhileSubscribed(5_000),
-            scope = viewModelScope
-        )
-
-    val question: StateFlow<Question?> = qna.filterNotNull()
-        .map { it.question }
+        .flatMapLatest { getQuestionUseCase(it) }
         .stateIn(
             initialValue = null,
             started = SharingStarted.WhileSubscribed(5_000),
@@ -49,8 +38,8 @@ class WriteAnswerViewModel(
     val answer = MutableStateFlow("")
 
     fun answerQna() = viewModelScope.launch {
-        val qna = qna.value ?: return@launch
-        answerQnaUseCase(qna, Answer(answer.value))
+        val question = question.value ?: return@launch
+        answerQnaUseCase(question.id, Answer(answer.value))
     }
 
     companion object {
