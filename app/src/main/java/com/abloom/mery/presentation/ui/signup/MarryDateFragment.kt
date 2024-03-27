76526a10bee6cfddfcc9d5726d54571edbfe050b
leeugun123 <leeugun123@naver.com>
2024-03-27 16:24:06 +0900

    [Feat/#41]: 날짜 저장 초기 구현

--- app/src/main/java/com/abloom/mery/presentation/ui/signup/MarryDateFragment.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/signup/MarryDateFragment.kt
@@ -2,6 +2,7 @@ package com.abloom.mery.presentation.ui.signup
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import com.abloom.mery.R
 import com.abloom.mery.databinding.FragmentMarryDateBinding
 import com.abloom.mery.presentation.common.base.BaseFragment
@@ -10,7 +11,11 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MarryDateFragment : BaseFragment<FragmentMarryDateBinding>(R.layout.fragment_marry_date) {
 
+    private val sharedViewModel: SharedViewModel by activityViewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
+
 }
