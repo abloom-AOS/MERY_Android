d8e4cc866a42f2a0cbd42333f136cc03a8474a4f
Choi YoonChan <choyc9239@gmail.com>
2024-03-27 15:39:11 +0900

    [Feat/#36] 질문카테고리 UI CategoryRecyclerListener 삭제

--- app/src/main/java/com/abloom/mery/presentation/ui/category/CategoryRecyclerListener.kt
+++
@@ -1,8 +0,0 @@
-package com.abloom.mery.presentation.ui.category
-
-import com.abloom.mery.categorytest.Question
-
-interface CategoryRecyclerListener {
-
-    fun onCategoryItemClick(question: Question)
-}
