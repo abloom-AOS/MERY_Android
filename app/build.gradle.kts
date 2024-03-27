1b9e8084a994e78fc532804d5b0fa859dd22fbcb
ki960213 <ki960213@gmail.com>
2024-03-27 21:40:46 +0900

    [Chore/#34] hilt navigation 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
 
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
+    implementation(libs.hilt.navigation.fragment)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
